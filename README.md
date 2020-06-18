ddc76d5e0ef2a1b9637605f4c900fef97d543f93
mardax007 <62964422+mardax007@users.noreply.github.com>
2020-06-18 21:39:58 +0200

    The code

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Face-detection-and-identification
 Face identification
+
+Requirements:
+opencv
+dlib
+face-recognition
+ecapture 
+argparse
+numpy 
+imutils
+pickle
+
+Usage:
+1. Put photo's in "dataset"
+2. Run: python train.py --dataset dataset --encodings encodings.pickle
+3.1. After that run: python detect_faces_video.py --prototxt deploy.prototxt.txt --model res10_300x300_ssd_iter_140000.caffemodel (optional: cnn or hog. default is hog. CNN is more accurate but HoG is faster.)
+Make sure your webcam is connected correctly. Sometimes it just doesn't see the webcam.
+3.2. Now the program will check if there is a person. If there is it will start the blink detection to check if the person lives. After that it will look up who it is.
+
+I am still working on making it more clear for others but I am now bussy at just making it work.
---
+++ blinkcheck.py
@@ -0,0 +1,90 @@
+import cv2
+import dlib
+from math import hypot
+import time
+import os
+from ecapture import ecapture as ec
+from cv2 import *
+
+
+cap = cv2.VideoCapture(0)
+
+
+def cb():
+    roundcount = 0
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+    countblinksformeplease = 0
+
+    sff = 0
+
+    def midpoint(p1, p2):
+        return int((p1.x + p2.x) / 2), int((p1.y + p2.y) / 2)
+
+    font = cv2.FONT_HERSHEY_PLAIN
+
+    def get_blinking_ratio(eye_points, facial_landmarks):
+        left_point = (facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y)
+        right_point = (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y)
+        center_top = midpoint(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2]))
+        center_bottom = midpoint(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4]))
+
+        hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
+        ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 2)
+
+        hor_line_lenght = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
+        ver_line_lenght = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
+
+        ratio = hor_line_lenght / ver_line_lenght
+        return ratio
+
+    while True:
+        _, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = detector(gray)
+        for face in faces:
+            # x, y = face.left(), face.top()
+            # x1, y1 = face.right(), face.bottom()
+            # cv2.rectangle(frame, (x, y), (x1, y1), (0, 255, 0), 2)
+
+            landmarks = predictor(gray, face)
+
+            left_eye_ratio = get_blinking_ratio([36, 37, 38, 39, 40, 41], landmarks)
+            right_eye_ratio = get_blinking_ratio([42, 43, 44, 45, 46, 47], landmarks)
+            blinking_ratio = (left_eye_ratio + right_eye_ratio) / 2
+
+            if blinking_ratio > 5.7 and sff == 1:
+                cv2.putText(frame, "BLINKING", (50, 150), font, 7, (255, 0, 0))
+                countblinksformeplease = countblinksformeplease + 1
+                print("blink: ")
+                print(countblinksformeplease)
+                sff = 0
+                time.sleep(0.5)
+
+            while blinking_ratio < 5.6 and sff == 0 and countblinksformeplease != 2:
+                time.sleep(1)
+                sff = 1
+                print("OPEN!")
+
+                break
+
+            while countblinksformeplease >= 2:
+                ec.capture(0, False, "person.png")
+                # er is een levend persoon
+                countblinksformeplease = 0
+                cv2.destroyAllWindows()
+                time.sleep(0.5)
+                print(countblinksformeplease)
+                cap.release()
+                os.system('python id.py --encodings encodings.pickle --image person.png')
+                quit()
+        cv2.imshow("Frame", frame)
+
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    quit(10000)
